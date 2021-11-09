3ef8414daeb7a4c94e3d029e8d096d844008c8b4
林毓庭 <91750181+yutinglin23@users.noreply.github.com>
2021-11-09 10:24:51 +0800

    w8-1蜂鳴器+LED

---
+++ w8-1.ino
@@ -0,0 +1,41 @@
+#include "pitches.h"
+
+int freq = 2000;
+int channel = 0;
+int resolution = 8;
+
+int tonePin = 12;
+
+int melody[] = {
+  NOTE_G5,NOTE_E5, NOTE_E5, NOTE_F5,NOTE_D5,NOTE_D5, NOTE_C5,NOTE_D5, NOTE_E5, NOTE_F5,NOTE_G5,NOTE_G5,NOTE_G5,0,
+  NOTE_G5,NOTE_E5, NOTE_E5, NOTE_F5,NOTE_D5,NOTE_D5,NOTE_C5,NOTE_E5, NOTE_G5, NOTE_G5,NOTE_E5,0,
+  NOTE_D5, NOTE_D5, NOTE_D5, NOTE_D5, NOTE_D5,NOTE_E5, NOTE_F5,NOTE_E5,NOTE_E5,NOTE_E5,NOTE_E5,NOTE_E5, NOTE_E5, NOTE_F5, NOTE_G5,0,
+  NOTE_G5,NOTE_E5, NOTE_E5, NOTE_F5,NOTE_D5,NOTE_D5,NOTE_C5,NOTE_E5, NOTE_G5, NOTE_G5, NOTE_C5,0,
+  
+  
+  
+};
+int noteDurations[] = {
+  4, 4, 2, 4, 4, 2, 4,4,4,2,4,4,4,4,
+  4, 4, 2, 4, 4, 2, 4,4,4,2,2,4,
+   4, 4, 4, 2, 4, 4, 2,4,4,4,2,4,4,2,4,
+ 4, 4, 2, 4, 4, 2, 4,4,4,2,2,4,
+  
+};
+
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(tonePin, channel);
+}
+
+void loop() {
+    for (int thisNote = 0; thisNote < 42; thisNote++) {
+      int noteDuration = 1000 / noteDurations[thisNote];
+      ledcWriteTone(channel, melody[thisNote]);
+      delay(noteDuration);
+      ledcWriteTone(channel, 0);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+    }
+}
